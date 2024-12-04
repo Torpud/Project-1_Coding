067b3f2c359085a53847146eb29c582bc12b4c2f
Torpud <torpud2009@gmail.com>
2024-12-04 16:08:41 +0700

    Add files via upload

---
+++ 1form_30Khongkwan.py
@@ -0,0 +1,8 @@
+print("Khongkwan Book is now open!")
+print("\nกรอกข้อมูล ชื่อ ชื่อหนังสือ วันที่เริ่มยืม และวันที่คืนได้เลยค่ะ")
+customer = input("ชื่อผู้ใช้บริการ")
+book = input("ชื่อหนังสือ")
+borrowing_date = input("วันที่เริ่มยืมหนังสือ")
+returning_date = input("วันที่คืนหนังสือ")
+print("คุณ ", customer, "\nยืมหนังสือ ", book, "เริ่มยืมหนังสือวันที่ ", borrowing_date, "ต้องคืนหนังสือวันที่ ", returning_date,)
+print("\nบันทึกข้อมูลเรียบร้อย ขอบคุณที่ใช้บริการค่ะ")
